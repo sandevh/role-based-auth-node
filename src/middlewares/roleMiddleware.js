9e27dc453e0b83548c5a4c36f88b8621d16a15a2
sandevh <sandevhettiarachchi@gmail.com>
2025-05-25 16:26:06 +0530

    created role based auth middleware to protect user routes

---
+++ src/middlewares/roleMiddleware.js
@@ -0,0 +1,11 @@
+const authorizeRole = (allowedRoles) => {
+  console.log("Allowed roles:", allowedRoles);
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({message: "Access denied"});
+    }
+    next();
+  }
+}
+
+export default authorizeRole;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,22 @@
 import { Router } from "express";
 import verifyToken from "../middlewares/authMiddleware.js";
+import authorizeRole from "../middlewares/roleMiddleware.js";
 
 const userRoutes = Router();
 
 // only admin can access
-userRoutes.get("/admin", verifyToken, (req, res) => {
+userRoutes.get("/admin", verifyToken, authorizeRole("admin"), (req, res) => {
   res.json({message: "Welcome Admin!"})
 })
 
 // both admin and manager can access
-userRoutes.get("/manager", verifyToken, (req, res) => {
+userRoutes.get("/manager", verifyToken, authorizeRole("admin, manager"), (req, res) => {
   res.json({ message: "Welcome Manager!" });
 });
 
 
 // all users can access
-userRoutes.get("/user", verifyToken, (req, res) => {
+userRoutes.get("/user", verifyToken, authorizeRole("admin, manager, user"), (req, res) => {
   res.json({ message: "Welcome User!" });
 });
 
